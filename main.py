2c5b7a7691396ca07472cdeff86348dcf354ae1a
Mark <23454mark@gmail.com>
2024-02-24 14:53:52 +0200

    3.0

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import lxml
 from bs4 import BeautifulSoup
 
 session = requests.Session()
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 
-for j in range(1, 7 ):
-    url = f"https://cash-backer.club/shops?page={j}"
+for j in range(1, 10):
+    url = f"https://rozetka.com.ua/notebooks/c80004/page={j}"
     print(f"PAGE")
     response = session.get(url, headers=header)
     if response.status_code == 200:
-            soup = BeautifulSoup(response.text, "lxml")
-            all_products = soup.find('div', class_="row col-lg-9 col-md-9 col-12")
-            products = all_products.find_all('div', class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
+        soup = BeautifulSoup(response.text, "lxml")
+        all_products = soup.find('ul', class_="catalog-grid ng-star-inserted")
+        products = all_products.find_all('div', class_="goods-tile__content")
 
-            for elem in products:
-                name_company = elem.find("div", class_="shop-title").text
-                procent = elem.find("div", class_="shop-rate").text
+        for elem in products:
+            try:
+                elem.find("div", class_="goods-tile__price--old price--gray ng-star-inserted")
+                get_name = elem.find("div", class_="goods-tile__title").text
+                get_price = elem.find("div", class_="goods-tile__price-value").text
 
                 with open("result.txt", "a", encoding="utf-8") as file:
-                    file.write(f"{name_company} -->> {procent}\n")
+                    file.write(f"{get_name} -->> {get_price}\n")
+            except:
+                print(f"{products} - знижки немає")
